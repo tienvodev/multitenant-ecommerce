bfb49b277cbc3d3546a8182e3962f16f8f539cc1
Tien Vo <tienhoangvo.dev@gmail.com>
2025-04-16 10:29:22 +0700

    styling: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">Hello world</p>
+    <div className="flex flex-col gap-y-4 p-4">
+      <div>
+        <Button variant="elevated">Button</Button>
+      </div>
+      <div>
+        <Input placeholder="Input" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="Textarea" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
